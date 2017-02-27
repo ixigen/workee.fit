a9a27663e0f6c7c7568e8c45ee8492c062823016
Adrian Oprea <adrian@oprea.rocks>
2017-02-27 15:57:58 +0200

    feat: Add logger logic / add default state for the store

--- app/main.js
+++ app/main.js
@@ -3,13 +3,19 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { exercises } from './exercises/ExerciseReducers'
+import createLogger from 'redux-logger';
+import exercises from './exercises/ExerciseReducers'
 import App from './App';
 
-const store = createStore(exercises, applyMiddleware(thunk));
+const logger = createLogger();
+const defaultState = {
+  exercises: [],
+};
+
+const store = createStore(exercises, defaultState, applyMiddleware(thunk, logger));
 
 render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <App />
   </Provider>,
   document.getElementById('root')
