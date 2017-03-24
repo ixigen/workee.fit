a5a7848797974c1c22df7d4df1fc3a6252a682ec
Adrian Oprea <adrian@oprea.rocks>
2017-03-24 15:26:10 +0200

    Simplify layout

--- app/workouts/Workout.js
+++ app/workouts/Workout.js
@@ -3,13 +3,12 @@ import './Workout.css';
 
 const Workout = ({ name, imageUrl, pdfSource, description }) => {
     return(
-        <div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-phone mdl-card mdl-shadow--2dp">
+<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-phone mdl-card mdl-shadow--2dp">
             <div className="mdl-card__title mdl-card--expand">
                 <h2 className="mdl-card__title-text" style={{textTransform: 'capitalize'}}>{name}</h2>
             </div>
             <div className="mdl-card__supporting-text mdl-grid">
                 <img className="workout-image mdl-cell mdl-cell--5-col" src={`${imageUrl}`} alt={name}/>
-                <div className="mdl-cell mdl-cell--5-col">{description}</div>
             </div>
             <div className="mdl-card__actions mdl-card--border">
                 <a href={`/workout/${name.split(' ').join('-')}`} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
